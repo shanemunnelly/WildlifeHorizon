795a38b7511c4c51d85cf69ee0cbb6912be3e42b
shanemunnelly <koboldcommander@gmail.com>
2024-02-06 20:03:49 +0000

    added ui for barrel interact

--- Wildlife Horizon/Assets/Scripts/FoodItemGenerator.cs
+++ Wildlife Horizon/Assets/Scripts/FoodItemGenerator.cs
@@ -2,11 +2,48 @@ using UnityEngine;
 
 public class FoodItemGenerator : MonoBehaviour
 {
-    [SerializeField] GameObject foodItemPrefab; // Drag and drop the food item prefab in the inspector
+    [SerializeField] GameObject foodItemPrefab;
+    [SerializeField] GameObject UI;
+    [SerializeField] GameObject interactButton;
+
+    private Camera mainCamera;
+    private RaycastHit hit;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        interactButton.SetActive(false); // Initially, hide the interact button
+    }
+
+    private void Update()
+    {
+        // Cast a ray from the main camera
+        Ray ray = mainCamera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            // Check if the ray hits the object attached to this script
+            if (hit.collider.gameObject == gameObject)
+            {
+                // Show the interact button
+                interactButton.SetActive(true);
+
+                // Check if the player presses the interact button
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    GenerateFoodItem();
+                }
+            }
+            else
+            {
+                // Hide the interact button if the player looks away
+                interactButton.SetActive(false);
+            }
+        }
+    }
 
     public void GenerateFoodItem()
     {
-        // Instantiate the food item prefab at the generator's position
-        Instantiate(foodItemPrefab, transform.position, Quaternion.identity);
-    }
+/*        Instantiate(foodItemPrefab, transform.position, Quaternion.identity);
+*/    }
 }
